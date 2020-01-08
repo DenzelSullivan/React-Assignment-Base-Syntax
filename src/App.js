9ef62a569982c52384db7d2500112914764b0c59
Sullivan, Denzel W <Denzel.Sullivan@dcsg.com>
2020-01-08 08:26:59 -0500

    Created UserInput and UserOutput Components. Add prop for output

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import UserInput from './Componets/UserInput';
+import UserOutput from './Componets/UserOutput'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1>React Base Syntax Assignment</h1>
+        <UserInput />
+        <UserOutput username="user"/>
+        <UserOutput username="user"/>
       </div>
     );
   }
---
+++ src/Componets/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = (props) => {
+    return( 
+        <div>
+            <p> Username: {props.username}</p>
+            <p> This is the second paragraph tag</p>
+        </div>
+    )
+}
+
+export default userOutput;
